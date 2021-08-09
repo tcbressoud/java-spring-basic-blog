4e39f8844ba4da0258d12c17b8548e42eb3125c5
T.C.Bressoud <bressoud@denison.edu>
2021-08-09 17:42:21 -0400

    module 2 changes: display static data

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,15 +1,26 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    public BlogController(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
     @RequestMapping("/" )
     public String listPosts(ModelMap themap) {
-        themap.put("title", "Blog Post 1");
+        List<Post> listposts = postRepository.getAllPosts();
+        themap.put("posts", listposts);
         return "home";
     }
 
